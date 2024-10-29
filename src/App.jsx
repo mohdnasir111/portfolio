f014b8a45290267d13b9bba550b7286f4d8ab451
mohdnasir111 <122935164+mohdnasir111@users.noreply.github.com>
2024-10-29 23:16:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Page1 from './pages/Page1'
+import Header from './components/Header'
+import Page2 from './pages/Page2'
+const App = () => {
+  return ( 
+   <> 
+
+<div id="main">
+  
+<Header /> 
+<Page1 />
+<Page2 />
+</div>
+
+</>
+  )
+}
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+const Header = () => {
+  return ( 
+   <>
+<div className='fixed w-full  flex items-center justify-end z-10 py-20 px-20'>
+    <button className='bg-black border-4 text-xl px-6 py-3 rounded-full  text-gray-400 hover:bg-slate-100 '> Hire Me</button>
+    <i className="ri-menu-search-fill text-4xl text-gray-200"></i>
+</div>
+</>
+
+
+  )
+}
+export default Header
